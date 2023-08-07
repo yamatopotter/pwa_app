4d2fdf4f87bb803622d316914b36c2b25119afe4
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2023-08-06 21:35:57 -0300

    wip: Aula 74

--- course-project/public/src/js/feed.js
+++ course-project/public/src/js/feed.js
@@ -1,3 +1,4 @@
+var sharedMomentsArea = document.querySelector("#shared-moments");
 var shareImageButton = document.querySelector("#share-image-button");
 var createPostArea = document.querySelector("#create-post");
 var closeCreatePostModalButton = document.querySelector(
@@ -6,21 +7,22 @@ var closeCreatePostModalButton = document.querySelector(
 
 function openCreatePostModal() {
   createPostArea.style.display = "block";
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
+  // Abrir a popup para instalar o software caso não tenha instalado
+  // if (deferredPrompt) {
+  //   deferredPrompt.prompt();
 
-    deferredPrompt.userChoice.then((c) => {
-      console.log(c.outcome);
+  //   deferredPrompt.userChoice.then((c) => {
+  //     console.log(c.outcome);
 
-      if (c.outcome === "dismissed") {
-        console.log("User cancelled installation");
-      } else {
-        console.log("User added to homescreen");
-      }
-    });
+  //     if (c.outcome === "dismissed") {
+  //       console.log("User cancelled installation");
+  //     } else {
+  //       console.log("User added to homescreen");
+  //     }
+  //   });
 
-    deferredPrompt = null;
-  }
+  //   deferredPrompt = null;
+  // }
 }
 
 function closeCreatePostModal() {
@@ -28,5 +30,33 @@ function closeCreatePostModal() {
 }
 
 shareImageButton.addEventListener("click", openCreatePostModal);
-
 closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
+
+function createCard() {
+  let cardWrapper = document.createElement("div");
+  cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
+  let cardTitle = document.createElement("div");
+  cardTitle.className = "mdl-card__title";
+  cardTitle.style.backgroundImage = "url('src/images/sf-boat.jpg')";
+  cardTitle.style.backgroundSize = "cover";
+  cardTitle.style.height = "180px";
+  cardWrapper.appendChild(cardTitle);
+  let cardTitleTextElement = document.createElement("h2");
+  cardTitleTextElement.style.color = "white";
+  cardTitleTextElement.className = "mdl-card__title-text";
+  cardTitleTextElement.textContent = "San Francisco Trip";
+  cardTitle.appendChild(cardTitleTextElement);
+  let cardSupportingText = document.createElement("div");
+  cardSupportingText.className = "mdl-card__supporting-text";
+  cardSupportingText.textContent = "In San Francisco";
+  cardSupportingText.style.textAlign = "center";
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch("https://httpbin.org/get")
+  .then((res) => {
+    return res.json;
+  })
+  .then((data) => createCard());
--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -1,13 +1,65 @@
+const CACHE_VERSION = "5";
+
 self.addEventListener("install", (event) => {
   console.log("[Service Worker] Installing Service Worker ...", event);
+  event.waitUntil(
+    caches.open(`static-v${CACHE_VERSION}`).then((cache) => {
+      console.log("[Service Worker] Precaching app shell");
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/promise.js",
+        "/src/js/fetch.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
+    })
+  );
 });
 
 self.addEventListener("activate", (event) => {
   console.log("[Service Worker] Activating Service Worker ...", event);
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (
+            key !== `static-v${CACHE_VERSION}` &&
+            key !== `dynamic-v${CACHE_VERSION}`
+          ) {
+            console.log("[Service Worker] Removing old cache.", key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
   return self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("[Service Worker] Fetching something ...", event);
-  event.respondWith(fetch(event.request));
+  // console.log("[Service Worker] Fetching something ...", event);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request)
+        .then((res) => {
+          return caches.open(`dynamic-v${CACHE_VERSION}`).then((cache) => {
+            cache.put(event.request.url, res.clone());
+            return res;
+          });
+        })
+        .catch((err) => {});
+    })
+  );
 });
